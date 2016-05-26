036335000cb8b68757eef5c9bb2e1f8ddd4f93cc
kaija <kaija.chang@gmail.com>
2016-05-27 00:28:41 +0800

    use scratch in route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var http = require('http');
-var request = require('request');
-target_url = process.env.ATS_URL;
-if ( !target_url ) {
-  target_url = 'http://127.0.0.1:9615/';
-}
+var scratch = require('../scratch.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 //  res.render('index', { title: 'Express' });
@@ -13,37 +10,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/metrics', function(req, res, next) {
-  request(target_url, {timeout:1000}, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      data = JSON.parse(body);
-      var out = "";
-      //res.set('Content-Type', 'text/plain');
-      for (var index in data.processes ){
-         var obj = data.processes[index];
-         var name = obj.name;
-         var cpu = obj.monit.cpu;
-         var mem = obj.monit.memory;
-         var restart = obj.pm2_env.restart_time;
-         var up = obj.pm2_env.status=="online"?1:0;
-         name=name.replace(/\-/g, '_');
-         name=name.replace(/\W/g, '_');
-         out += "pm2_"+name+"_cpu " + cpu + "\n";
-         out += "pm2_"+name+"_memory " + mem + "\n";
-         out += "pm2_"+name+"_restart " + restart + "\n";
-         out += "pm2_"+name+"_online " + up + "\n";
-         for (var m in obj.pm2_env.axm_monitor) {
-           if (m != "Loop delay") {
-             metric = obj.pm2_env.axm_monitor[m];
-             m=m.replace(/\-/g, '_');
-             m=m.replace(/\W/g, '_');
-             value = parseInt(metric.value);
-             out += "pm2_"+name+"_"+m+" " +value + "\n";
-           }
-         }
-      }
-      res.set('Content-Type', 'text/plain');
-      res.send(out);
+  scratch.collect(function(data){
+    res.set('Content-Type', 'text/plain');
+    var out = "";
+    for (var i in data) {
+      out += i + " " + data[i] + "\n";
     }
+    res.send(out);
   });
 });
 
--- scratch.js
+++ scratch.js
@@ -3,7 +3,7 @@ target_url = process.env.ATS_URL;
 if ( !target_url ) {
   target_url = 'http://127.0.0.1:9615/';
 } 
-exports.grab = function(cb){
+exports.collect = function(cb){
   request(target_url, {timeout:1000}, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       data = JSON.parse(body);
@@ -18,21 +18,21 @@ exports.grab = function(cb){
         var up = obj.pm2_env.status=="online"?1:0;
         name=name.replace(/\-/g, '_');
         name=name.replace(/\W/g, '_');
-        out.push({"pm2_"+name+"_cpu",  cpu });
-        out.push({"pm2_"+name+"_memory", mem });
-        out.push({"pm2_"+name+"_restart" , restart});
-        out.push({"pm2_"+name+"_online", up });
+        out["pm2_"+name+"_cpu"] = cpu;
+        out["pm2_"+name+"_memory"] = mem;
+        out["pm2_"+name+"_restart"] = restart;
+        out["pm2_"+name+"_online"] = up;
         for (var m in obj.pm2_env.axm_monitor) {
           if (m != "Loop delay") {
             metric = obj.pm2_env.axm_monitor[m];
             m=m.replace(/\-/g, '_');
             m=m.replace(/\W/g, '_');
             value = parseFloat(metric.value);
-            out.push({ "pm2_"+name+"_"+m , value });
+            out["pm2_"+name+"_"+m] = value;
           }
         }
-        cb(out); 
       }
+      cb(out); 
     }
   });
 }
